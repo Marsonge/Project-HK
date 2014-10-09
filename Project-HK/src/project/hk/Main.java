181900b857d01cd33877eac0d84af08c0ce6887d
Marsonge <tim.caillot@gmail.com>
2014-10-09 11:50:59 +0200

    Random from pool [NEEDS TESTING]
    RandomFromPool should get a random character from the pool, add it to
    the current pool, and never ever choose the same twice. It could loop
    indefinitly if you have the max number of inhabitants and a migration
    occurs. Will fix somehow

--- Project-HK/src/project/hk/Main.java
+++ Project-HK/src/project/hk/Main.java
@@ -11,6 +11,7 @@ import eventflags.FlagSetter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 /**
  *
  * @author Tim
@@ -26,12 +27,45 @@ public class Main {
         Conteneur c = new Conteneur();
         c.charger("flagpool");
         FlagSetter flgst = new FlagSetter();
+        CharConteneur co = new CharConteneur();
+        co.charger("charpool");
+        ArrayList<Integer> chosen = new ArrayList<>();
+        CharConteneur current = new CharConteneur();
+        current.add(randomFromPool(chosen, co)); // We select 4 random character
+        current.add(randomFromPool(chosen, co));
+        current.add(randomFromPool(chosen, co));
+        current.add(randomFromPool(chosen, co));
+        
+    }
+    private Character randomFromPool(ArrayList<Integer> chosen,CharConteneur co)
+    {
+        Character ch;
+        int i;
+        Random rand = new Random();
+        do
+        {
+            i = rand.nextInt(co.nbElements()+1);
+            
+        } while(this.belongs(i,chosen));
+        ch = (Character) co.obtenir(i);
+        chosen.add(i);
+        return ch;
+    }
+    private boolean belongs(int i,ArrayList<Integer>  chosen)
+    {
+        int j = 0;
+        while(j<chosen.size())
+        {
+             if (chosen.get(j) == i)
+             {
+                return true;
+             }
+        }
+        return false;    
+    
     }
     public static void main(String[] args) throws IOException {
-        Character c = new Character(0,"Doc 'Von' Marsong",0,20,0,0,1);
-        CharConteneur co = new CharConteneur();
-        co.add(c);
-        co.sauvegarder("charpool");
+        
     }
     
 }
