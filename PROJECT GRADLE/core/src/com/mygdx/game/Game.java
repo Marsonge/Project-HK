5fd3682ccdd1d8923c05fc6f1c1d3be848dd62a8
Submoon <fabien.bal-fontaine@etu.univ-lyon1.fr>
2014-12-04 16:33:02 +0100

    Cliquez pour un jolof!
    J'ai rajouté la classe batiment, qui créé un batiment avec une texture
    en arg, la position, etc...
    (extends Rectangle)
    Et une liste de batiment dont l'affichage se rafraichit !

--- PROJECT GRADLE/core/src/com/mygdx/game/Game.java
+++ PROJECT GRADLE/core/src/com/mygdx/game/Game.java
@@ -2,12 +2,16 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.mygdx.game.entities.Building;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -16,10 +20,15 @@ public class Game extends ApplicationAdapter {
         private OrthographicCamera camera;
         private Rectangle jolof;
         private Rectangle jolof1;
+        private Building bat1;
+        private List<Building> buildlist;
+        private int newx;
+        private int newy;
         
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+                buildlist= new ArrayList();
 		img = new Texture("jolof.png");
                 bg = new Texture("bg.png");
                 camera = new OrthographicCamera();
@@ -33,6 +42,8 @@ public class Game extends ApplicationAdapter {
                 jolof1.y = 300;
                 jolof1.width = 64;
                 jolof1.height = 64;
+                bat1=new Building(1,new Texture("jolof.png"),100,100);
+                buildlist.add(bat1);
 	}
 
 	@Override
@@ -45,11 +56,19 @@ public class Game extends ApplicationAdapter {
                 batch.draw(bg,0,0);
                 batch.draw(img, jolof.x, jolof.y);
                 batch.draw(img, jolof1.x, jolof1.y);
+                for(Building batiment : buildlist){
+                    batch.draw(batiment.sprite, batiment.x, batiment.y);
+                }
 		batch.end();
                 if(Gdx.input.isKeyPressed(Keys.LEFT)) jolof.x -= 200 * Gdx.graphics.getDeltaTime();
                 if(Gdx.input.isKeyPressed(Keys.RIGHT)) jolof.x += 200 * Gdx.graphics.getDeltaTime();
                 if(Gdx.input.isKeyPressed(Keys.DOWN)) jolof.y -= 200 * Gdx.graphics.getDeltaTime();
                 if(Gdx.input.isKeyPressed(Keys.UP)) jolof.y += 200 * Gdx.graphics.getDeltaTime();
+                if(Gdx.input.isButtonPressed(Buttons.LEFT)) {
+                    newx = Gdx.input.getX()-64/2;
+                    newy = Gdx.input.getY()+64/2;
+                    buildlist.add(new Building(1,new Texture("jolof.png"),newx,480-newy));
+                }//Quand on clique, on fait apparaitre un batiment avec l'image de jolof!
                 if(jolof.x < 0) jolof.x = 0;
                 if(jolof.x > 800 - 64) jolof.x = 800 - 64;
                 if(jolof.y < 0) jolof.y = 0;
--- PROJECT GRADLE/core/src/com/mygdx/game/entities/Building.java
+++ PROJECT GRADLE/core/src/com/mygdx/game/entities/Building.java
@@ -6,21 +6,24 @@
 package com.mygdx.game.entities;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 
 /**
  *
  * @author Fabien
  */
-public class Building {
-    Texture sprite;
-    int id;
-    int x;
-    int y;
+public class Building extends Rectangle{
+    public Texture sprite;
+    public int id;
     
-    public Building(int type, Texture sprite,int x, int y){
-        id=type;
-        this.sprite=sprite;
+    public Building(int type, Texture sprite, int x, int y){
+        super();
         this.x=x;
         this.y=y;
+        id=type;
+        this.sprite=sprite;
+        width=64;
+        height=64;
+        
     }
 }
